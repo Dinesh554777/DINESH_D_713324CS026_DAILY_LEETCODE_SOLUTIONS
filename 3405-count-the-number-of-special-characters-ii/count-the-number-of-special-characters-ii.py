fba415112675ef1b5b75663d6f8d55b5674115fb
Dinesh D <dinesh6212193@gmail.com>
2026-05-27 21:47:28 +0530

    Time: 327 ms (35.88%) | Memory: 21.4 MB (74.71%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.py
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.py
@@ -1,18 +1,28 @@
-# Added using AI
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        mpl = [-1] * 26
-        mpu = [-1] * 26
-        for i, a in enumerate(word) :
-            if a.islower():
-                mpl[ord(a) - ord('a')] = i
+        last_lower = {}
+        first_upper = {}
+        invalid = set()
+
+        for i, ch in enumerate(word):
+            letter = ch.lower()
+
+            if ch.islower():
+                last_lower[letter] = i
+
+                # lowercase appeared after uppercase
+                if letter in first_upper:
+                    invalid.add(letter)
+
             else:
-                idx = ord(a) - ord('A')
-                if mpu[idx] == -1 :
-                    mpu[idx] = i
-        ans = 0
-        for i in range(26) :
-            if mpl[i] != -1 and mpu[i] != -1:
-                if mpl[i] < mpu[i]:
-                    ans += 1
-        return ans
+                # store only first uppercase occurrence
+                if letter not in first_upper:
+                    first_upper[letter] = i
+
+        special_count = 0
+
+        for letter in last_lower:
+            if letter in first_upper and letter not in invalid:
+                special_count += 1
+
+        return special_count
